e6291f81d1b014ae2dc2f3ea2ea6e27605e572da
rodp.dev <airobo.software>
2021-08-19 00:24:41 +1000

    Add control

---
+++ src/Control/Core/LayoutAxisTypeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Rox
+{
+    /// <summary>
+    /// Type converter for a LayoutAxis which represents either a X or Y axis.
+    /// </summary>
+    [TypeConversion(typeof(LayoutAxis))]
+    public class LayoutAxisTypeConverter
+        : TypeConverter
+    {
+        #region Convert
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string stringValue)
+            {
+                string[] axisArray = stringValue.Split(',');
+                if (axisArray.Length == 2 &&
+                    double.TryParse(axisArray[0], NumberStyles.Number, culture, out double point) &&
+                    double.TryParse(axisArray[1], NumberStyles.Number, culture, out double measure))
+                {
+                    return new LayoutAxis(point, measure);
+                }
+            }
+
+            throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(LayoutAxis)));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Control/Core/LayoutRectangleTypeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Rox
+{
+    /// <summary>
+    /// Type converter for a LayoutRectangle which represents Fit or Flow rectangle.
+    /// </summary>
+    [TypeConversion(typeof(LayoutRectangle))]
+    public class LayoutRectangleTypeConverter
+        : TypeConverter
+    {
+        #region Convert
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string stringValue)
+            {
+                string[] rectangleArray = stringValue.Split(',');
+                if (rectangleArray.Length == 4
+                    && double.TryParse(rectangleArray[0], NumberStyles.Number, CultureInfo.InvariantCulture, out double fitPoint)
+                    && double.TryParse(rectangleArray[1], NumberStyles.Number, CultureInfo.InvariantCulture, out double flowPoint)
+                    && double.TryParse(rectangleArray[2], NumberStyles.Number, CultureInfo.InvariantCulture, out double fitMeasure)
+                    && double.TryParse(rectangleArray[3], NumberStyles.Number, CultureInfo.InvariantCulture, out double flowMeasure))
+                {
+                    return new LayoutRectangle(fitPoint, flowPoint, fitMeasure, flowMeasure);
+                }
+            }
+
+            throw new InvalidOperationException(string.Format("Cannot convert \"{0}\" into {1}", value, typeof(LayoutRectangle)));
+        }
+
+        #endregion
+    }
+}
